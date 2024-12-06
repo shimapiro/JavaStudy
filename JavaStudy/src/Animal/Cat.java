52a78eb8cba1f945e74f5cce3273bddbd5b78779
jtobo <jtobo@host.docker.internal>
2024-12-07 00:24:03 +0900

    オブジェクト指向の勉強

---
+++ JavaStudy/src/Animal/Cat.java
@@ -0,0 +1,18 @@
+package Animal;
+
+public class Cat extends Animal {
+    private final String catColor;
+
+    public Cat(String name, int age, String catColor) {
+        super(name, age);
+        this.catColor = catColor;
+    }
+
+    public String getColor() {
+        return catColor;
+    }
+    @Override
+    public void makeSound() {
+        System.out.println(getName() + "がにゃーと鳴いた");
+    }
+}
