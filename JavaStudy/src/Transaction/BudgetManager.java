48c534ea6f35487493aff8a9d6de277fe46f0d2e
shimapiro <vjtoboku@gmail.com>
2025-01-05 02:33:58 +0900

    家計簿アプリ

--- JavaStudy/src/Transaction/BudgetManager.java
+++ JavaStudy/src/Transaction/BudgetManager.java
@@ -2,7 +2,6 @@ package Transaction;
 
 import java.util.ArrayList;
 
-
 public class BudgetManager {
 
 	private ArrayList<Transaction> transactions;
--- JavaStudy/src/Transaction/Main.java
+++ JavaStudy/src/Transaction/Main.java
@@ -2,7 +2,6 @@ package Transaction;
 
 import java.util.Scanner;
 
-
 public class Main {
 
 	public static void main(String[] args) {
